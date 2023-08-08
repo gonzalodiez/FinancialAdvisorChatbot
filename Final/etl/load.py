604afc9e9c4f2b80c3b280650ec575954ac6244d
Micaela Kortsarz <micaelakortsarz@gmail.com>
2023-08-08 19:14:45 -0300

    Update load.py into function

--- Final/etl/load.py
+++ Final/etl/load.py
@@ -4,30 +4,34 @@ from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings, SentenceTransformerEmbeddings
 import pandas as pd
 
+def process_documents():
+    df = pd.read_csv(os.environ['DATASET_NAME'])
 
-df=pd.read_csv('dataset_v2.csv')
-
-r_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=2000,
-    chunk_overlap=100 ,
-    separators=["\n\n", "\n", "(?<=\. )", " ", ""],
-    length_function=len
+    r_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=100,
+        separators=["\n\n", "\n", "(?<=\. )", " ", ""],
+        length_function=len
     )
 
-# Use document column as text and turn it into strings
-splits=df.document.tolist()
-splits_strings = [str(elemento) for elemento in splits]
+    # Use document column as text and turn it into strings
+    splits = df.document.tolist()
+    splits_strings = [str(element) for element in splits]
 
-# Use the rest of the columns as metadata
-metadata=df.drop(columns='document', axis=1)
-metadata=metadata.to_dict(orient='records')
+    # Use the rest of the columns as metadata
+    metadata = df.drop(columns='document', axis=1)
+    metadata = metadata.to_dict(orient='records')
 
-assert len(splits_strings)==len(metadata)
+    assert len(splits_strings) == len(metadata)
 
-embeddings = HuggingFaceEmbeddings(model_name="multi-qa-MiniLM-L6-cos-v1")
+    embeddings = HuggingFaceEmbeddings(model_name="multi-qa-MiniLM-L6-cos-v1")
 
-db = Chroma.from_documents(
-    documents=splits,
-    persist_directory="chroma",   
-    embedding=embeddings
-)
+    db = Chroma.from_documents(
+        documents=splits,
+        persist_directory="chroma",
+        embedding=embeddings
+    )
+
+# This allows the module to be run as a standalone script as well
+if __name__ == '__main__':
+    process_documents()
