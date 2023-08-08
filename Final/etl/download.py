6fc34f874db608bb5b93dc929adb3cec3036201a
Micaela Kortsarz <micaelakortsarz@gmail.com>
2023-08-08 19:10:24 -0300

    Update download.py into function

--- Final/etl/download.py
+++ Final/etl/download.py
@@ -1,54 +1,55 @@
-# import boto3
-# from getpass import getpass
-# import os 
-
 import boto3
 import os
-from config import *
-# Define AWS credentials
-aws_access_key_id = 'YOUR_ACCESS_KEY_ID'
-aws_secret_access_key = 'YOUR_SECRET_ACCESS_KEY'
 
-# Create an S3 client
-s3_client = boto3.client(
-    's3',
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key
-)
+def download_from_s3():
+    # Read AWS credentials from environment variables
+    aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+    aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 
-# Create the paginator for the list_objects_v2 API
-paginator = s3_client.get_paginator('list_objects_v2')
+    # Create an S3 client
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
 
-# Specify the bucket name and prefix (directory path)
-bucket_name = 'your-bucket-name'
-prefix = 'your-prefix/'
+    # Create the paginator for the list_objects_v2 API
+    paginator = s3_client.get_paginator('list_objects_v2')
 
-# List objects within the bucket using the paginator, limiting to 999 objects
-response_iterator = paginator.paginate(
-    Bucket=bucket_name,
-    Prefix=prefix,
-    PaginationConfig={'MaxItems': 1000}
-)
+    # Read bucket name and prefix from environment variables
+    bucket_name = os.environ['BUCKET_NAME']
+    prefix = os.environ['PREFIX']
 
-# Specify the local directory path to save the files
-local_directory = Local_directory
+    # List objects within the bucket using the paginator, limiting to 999 objects
+    response_iterator = paginator.paginate(
+        Bucket=bucket_name,
+        Prefix=prefix,
+        PaginationConfig={'MaxItems': 1000}
+    )
 
-# Create the local directory if it doesn't exist
-os.makedirs(local_directory, exist_ok=True)
+    # Read local directory path from environment variable
+    local_directory = os.environ['LOCAL_DIRECTORY']
 
-# Download each file to the local directory
-for response in response_iterator:
-    if 'Contents' in response:
-        for obj in response['Contents']:
-            key = obj['Key']
-            
-            # Skip directories
-            if key.endswith('/'):
-                continue
-            
-            local_file_path = os.path.join(local_directory, os.path.basename(key))
-            
-            # Download the file from S3 to the local path
-            s3_client.download_file(bucket_name, key, local_file_path)
-            
-            print(f'Downloaded: {key} -> {local_file_path}')
+    # Create the local directory if it doesn't exist
+    os.makedirs(local_directory, exist_ok=True)
+
+    # Download each file to the local directory
+    for response in response_iterator:
+        if 'Contents' in response:
+            for obj in response['Contents']:
+                key = obj['Key']
+                
+                # Skip directories
+                if key.endswith('/'):
+                    continue
+                
+                local_file_path = os.path.join(local_directory, os.path.basename(key))
+                
+                # Download the file from S3 to the local path
+                s3_client.download_file(bucket_name, key, local_file_path)
+                
+                print(f'Downloaded: {key} -> {local_file_path}')
+
+# This allows the module to be run as a standalone script as well
+if __name__ == '__main__':
+    download_from_s3()
