6811796b227c95dc9e0e2ca8f58b28ef3307b6e3
Micaela Kortsarz <micaelakortsarz@gmail.com>
2023-08-08 21:58:55 -0300

    commented load.py

--- Final/etl/load.py
+++ Final/etl/load.py
@@ -1,3 +1,4 @@
+# Importing necessary modules for processing documents
 from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
@@ -5,38 +6,47 @@ from langchain.embeddings import HuggingFaceEmbeddings, SentenceTransformerEmbed
 import pandas as pd
 from dotenv import load_dotenv
 import os
+
 # Load environment variables from the .env file
 load_dotenv()
 
 def process_documents():
+    """ Processes documents from a CSV file and stores them in a Chroma database.
+
+    This function reads the dataset from the specified file path, extracts the documents,
+    and the metadata, and then stores them in a Chroma database with embeddings.
+    """
+    # Construct the file path using environment variables
     file_path = os.path.join(os.environ['LOCAL_DIRECTORY'], os.environ['DATASET_NAME'])
+    
+    # Read the CSV file into a DataFrame
     df = pd.read_csv(file_path)
 
-    r_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=2000,
-        chunk_overlap=100,
-        separators=["\n\n", "\n", "(?<=\. )", " ", ""],
-        length_function=len
-    )
-
-    # Use document column as text and turn it into strings
+    # Extract the document column as text and convert to strings
     splits = df.document.tolist()
     splits_strings = [str(element) for element in splits]
 
-    # Use the rest of the columns as metadata
+    # Extract the rest of the columns as metadata
     metadata = df.drop(columns='document', axis=1)
     metadata = metadata.to_dict(orient='records')
 
+    # Ensure the text and metadata have the same length
     assert len(splits_strings) == len(metadata)
 
+    # Initialize the embeddings using a specific model
     embeddings = HuggingFaceEmbeddings(model_name="multi-qa-MiniLM-L6-cos-v1")
 
-    db = Chroma.from_documents(
-        documents=splits,
-        persist_directory="chroma",
-        embedding=embeddings
-    )
+    # Create a Chroma database from the texts, metadata, and embeddings
+    db = Chroma.from_texts(
+        texts=splits_strings,
+        metadatas=metadata,
+        persist_directory=os.environ['PERSIST_DIRECTORY'],
+        embedding=embeddings,
+        collection_name=os.environ['COLLECTION']
+        )
 
-"""# This allows the module to be run as a standalone script as well
+# Usage example
+"""
 if __name__ == '__main__':
-    process_documents()"""
+    process_documents()
+"""
